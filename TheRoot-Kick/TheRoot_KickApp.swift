a95f01cdd9ab707509d4366a2d584c8b62654670
hsj._.06 <gtw030488@gmail.com>
2023-06-18 20:41:51 +0900

    Initial Commit

---
+++ TheRoot-Kick/TheRoot_KickApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheRoot_KickApp.swift
+//  TheRoot-Kick
+//
+//  Created by 홍승재 on 2023/06/18.
+//
+
+import SwiftUI
+
+@main
+struct TheRoot_KickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
